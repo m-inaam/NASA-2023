89ecee7b85de7a5363ddc448cfd6ec1d345204f6
Muhammad Inaamullah <94857175+m-inaam@users.noreply.github.com>
2023-10-08 16:56:48 +0500

    final-api

--- Inaam/OMNI/ai_API/model_api.py
+++ Inaam/OMNI/ai_API/model_api.py
@@ -1,16 +1,20 @@
-import os
 import numpy as np
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 from tensorflow import keras
-from sklearn.preprocessing import MinMaxScaler
 
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+# Enable CORS for all routes
+CORS(app, resources={r'*': {'origins': '*'}})
 
 # Load the pre-trained model
 model = keras.models.load_model('omni_rnn_0.h5')
 
-# Create a scaler for input data
-scaler = MinMaxScaler()
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -24,18 +28,14 @@ def predict():
             return jsonify({"error": "Input data should be a list"}), 400
 
         # Ensure the input data has the correct number of features
-        if len(input_data) != 3:  # Adjust the number of features as needed
+        if len(input_data) != 3:
             return jsonify({"error": "Input data should have 3 features"}), 400
 
-        # Convert input data to a NumPy array and scale it
+        # Convert input data to a NumPy array
         input_data = np.array(input_data).reshape(1, -1)
-        input_data = scaler.transform(input_data)
 
         # Make predictions using the loaded model
         predictions = model.predict(input_data)
-
-        # Inverse transform the scaled predictions to the original scale
-        predictions = scaler.inverse_transform(predictions)
 
         # Format the predictions as a dictionary
         result = {
